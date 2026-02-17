be8ea01777a639088708f58a8f85318415b0aa2d
Sunyanan Choochotkaew <sunyanan.choochotkaew1@ibm.com>
2026-02-17 16:35:26 +0900

    feat: initial commit v1.2.0
    Co-authored-by: Sunyanan Choochotkaew <sunyanan.choochotkaew1@ibm.com>
    Co-authored-by: Laurentiu Bradin <laurentiu.bradin@ibm.com>
    Co-authored-by: Avishek Goswami <avishek.goswami@ibm.com>
    Signed-off-by: Sunyanan Choochotkaew <sunyanan.choochotkaew1@ibm.com>

---
+++ pkg/types/pci_device.go
@@ -0,0 +1,152 @@
+/*
+ * Copyright (c) 2022 IBM Corp. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package types
+
+import (
+	"strconv"
+
+	cst "github.com/ibm-aiu/dra-driver-spyre/pkg/const"
+	"github.com/ibm-aiu/dra-driver-spyre/pkg/utils"
+	"github.com/jaypipes/ghw"
+	klog "k8s.io/klog/v2"
+	pluginapi "k8s.io/kubelet/pkg/apis/deviceplugin/v1beta1"
+	cdispec "tags.cncf.io/container-device-interface/specs-go"
+)
+
+// pciDevice common abstract for Spyre PCI device
+type pciDevice struct {
+	basePciDevice *ghw.PCIDevice
+	pfAddr        string
+	driver        string
+	vfID          int
+	numa          string
+	apiDevice     *pluginapi.Device
+}
+
+// Convert NUMA node number to string.
+// A node of -1 represents "unknown" and is converted to the empty string.
+func nodeToStr(nodeNum int) string {
+	if nodeNum >= 0 {
+		return strconv.Itoa(nodeNum)
+	}
+	return ""
+}
+
+// NewPciSpyreDevice returns an instance of PciDevice interface
+func NewPciSpyreDevice(dev *ghw.PCIDevice) (PciDevice, error) {
+	pciAddr := dev.Address
+
+	// Get PF PCI address
+	pfAddr, err := utils.GetPfAddr(pciAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	// Get driver info
+	driverName, err := utils.GetDriverName(pciAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	vfID, err := utils.GetVFID(pciAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	nodeNum := utils.GetDevNode(pciAddr)
+	apiDevice := &pluginapi.Device{
+		ID:     pciAddr,
+		Health: pluginapi.Healthy,
+	}
+	if nodeNum >= 0 {
+		numaInfo := &pluginapi.NUMANode{
+			ID: int64(nodeNum),
+		}
+		apiDevice.Topology = &pluginapi.TopologyInfo{
+			Nodes: []*pluginapi.NUMANode{numaInfo},
+		}
+	}
+
+	// 	Create pciAiuDevice object with all relevant info
+	return &pciDevice{
+		basePciDevice: dev,
+		pfAddr:        pfAddr,
+		driver:        driverName,
+		vfID:          vfID,
+		apiDevice:     apiDevice,
+		numa:          nodeToStr(nodeNum),
+	}, nil
+}
+
+func (pd *pciDevice) GetPfPciAddr() string {
+	return pd.pfAddr
+}
+
+func (pd *pciDevice) GetVendor() string {
+	return pd.basePciDevice.Vendor.ID
+}
+
+func (pd *pciDevice) GetProductID() string {
+	return pd.basePciDevice.Product.ID
+}
+
+func (pd *pciDevice) GetPciAddr() string {
+	return pd.basePciDevice.Address
+}
+
+func (pd *pciDevice) GetDriver() string {
+	return pd.driver
+}
+
+func (pd *pciDevice) IsSriovPF() bool {
+	return false
+}
+
+func (pd *pciDevice) GetSubClass() string {
+	return pd.basePciDevice.Subclass.ID
+}
+
+func (pd *pciDevice) GetCDIDeviceSpec() []*cdispec.DeviceNode {
+	devSpecs := make([]*cdispec.DeviceNode, 0)
+	devSpecs = append(devSpecs, &cdispec.DeviceNode{
+		HostPath:    cst.VfioMount,
+		Path:        cst.VfioMount,
+		Permissions: "mrw",
+	})
+	pciAddr := pd.GetPciAddr()
+	vfioDevHost, vfioDevContainer, err := utils.GetVFIODeviceFile(pciAddr)
+	if err != nil {
+		klog.Errorf("GetCDIDeviceSpec(): error getting vfio device file for device: %s, %s", pciAddr, err.Error())
+	} else {
+		devSpecs = append(devSpecs, &cdispec.DeviceNode{
+			HostPath:    vfioDevHost,
+			Path:        vfioDevContainer,
+			Permissions: "mrw",
+		})
+	}
+	return devSpecs
+}
+
+func (pd *pciDevice) GetEnvVal() string {
+	return pd.GetPciAddr()
+}
+
+func (pd *pciDevice) GetMounts() []*pluginapi.Mount {
+	mnt := make([]*pluginapi.Mount, 0)
+	return mnt
+}
+
+func (pd *pciDevice) GetAPIDevice() *pluginapi.Device {
+	return pd.apiDevice
+}
+
+func (pd *pciDevice) GetVFID() int {
+	return pd.vfID
+}
+
+func (pd *pciDevice) GetNumaInfo() string {
+	return pd.numa
+}
---
+++ test/unit-test/handler/config_test.go
@@ -0,0 +1,90 @@
+// Copyright 2026 IBM Corp.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cdi_test
+
+import (
+	"os"
+
+	"github.com/ibm-aiu/dra-driver-spyre/pkg/types"
+	. "github.com/ibm-aiu/dra-driver-spyre/pkg/utils"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	cdiapi "tags.cncf.io/container-device-interface/pkg/cdi"
+	cdispec "tags.cncf.io/container-device-interface/specs-go"
+)
+
+var _ = Describe("Senlib Config", Ordered, func() {
+	pciAddresses := []string{"0000:1a:00.0", "0000:1c:00.0"}
+
+	Context("init/prepare - CleanZombieConfigFolders", func() {
+		mnts := []*cdispec.Mount{}
+		devices := getPreparedDevices(pciAddresses)
+
+		It("should remove zombie config", func() {
+			mnts = GenerateSenlibConfig(pciAddresses)
+			CheckPathExists(mnts[0].HostPath, true)
+			By("clean zombie config")
+			err := cdiHandler.CleanZombieConfigFolders()
+			Expect(err).To(BeNil())
+			CheckPathExists(mnts[0].HostPath, false)
+		})
+
+		It("should not remove non-zombie config", func() {
+			By("create spec")
+			claimUID := generateUID()
+			_, err := cdiHandler.CreateClaimSpecFile(claimUID, types.ProductIDPf, devices)
+			Expect(err).To(BeNil())
+			spec, err := cdiHandler.ReadSpec(claimUID)
+			Expect(err).To(BeNil())
+			By("clean zombie config")
+			err = cdiHandler.CleanZombieConfigFolders()
+			Expect(err).To(BeNil())
+			for _, mnt := range spec.ContainerEdits.Mounts {
+				if mnt.ContainerPath == GetConfigContainerPath() ||
+					mnt.ContainerPath == GetMetricsContainerPath() {
+					CheckPathExists(mnt.HostPath, true)
+				}
+			}
+		})
+	})
+
+	It("prepare - unprepare", func() {
+		mnts := GenerateSenlibConfig(pciAddresses)
+		CheckPathExists(mnts[0].HostPath, true)
+		spec := &cdiapi.Spec{
+			Spec: &cdispec.Spec{
+				ContainerEdits: cdispec.ContainerEdits{
+					Mounts: mnts,
+				},
+			},
+		}
+		By("delete config")
+		cdiHandler.DeleteConfigFile(spec)
+		CheckPathExists(mnts[0].HostPath, false)
+	})
+})
+
+func GenerateSenlibConfig(pciAddresses []string) []*cdispec.Mount {
+	mnts, err := configHandler.GetConfigMetricsMount(types.ProductIDPf, pciAddresses)
+	Expect(err).To(BeNil())
+	// config and metrics
+	Expect(len(mnts)).To(Equal(2))
+	return mnts
+}
+
+func CheckPathExists(hostpath string, exist bool) {
+	_, err := os.Stat(hostpath)
+	Expect(os.IsNotExist(err)).To(Equal(!exist))
+}
